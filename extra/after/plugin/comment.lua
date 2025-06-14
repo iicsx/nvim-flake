d1969b8b8d04df1e037a9efe912cfd744a1e2db0
iicsx <maxinies00@gmail.com>
2025-06-15 01:01:06 +0200

    update(comments): add more convenient remaps to comment selections

--- extra/after/plugin/comment.lua
+++ extra/after/plugin/comment.lua
@@ -1,1 +1,12 @@
-require("Comment").setup()
+require("Comment").setup({
+    toggler = {
+        ---Line-comment toggle keymap
+        line = '<leader>cl',
+        ---Block-comment toggle keymap
+        block = '<leader>cb',
+    },
+    extra = {
+        ---Add comment at the end of line
+        eol = '<C-a>',
+    },
+})
