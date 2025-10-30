06c37baa49a026061f9c9c06372d384e0da98667
iicsx <maxinies00@gmail.com>
2025-10-30 09:36:30 +0100

    update(init): make changing colors easier and more error resilient

--- extra/init.lua
+++ extra/init.lua
@@ -1,7 +1,7 @@
 local function module_exists(name)
-  local ok, _ = pcall(require, name)
+	local ok, _ = pcall(require, name)
 
-  return ok
+	return ok
 end
 
 require("set")
@@ -9,19 +9,40 @@ require("remap")
 
 WITH_CATS = module_exists("nixCats")
 if not WITH_CATS then
-  require("lze")
+	require("lze")
 end
 
-DEFAULT_COLOR = "sonokai"
+local theme_index = 3
+local themes = {
+	"poimandres",
+	"NeoSolarized",
+	"oh-lucy",
+	"sonokai",
+	"tokyonight",
+}
+
+DEFAULT_COLOR = themes[theme_index]
 DEFAULT_BAR_COLOR = "none"
 
 vim.diagnostic.config({
-  virtual_text = {
-    prefix = '●',
-  },
+	virtual_text = {
+		prefix = "●",
+	},
 })
 
-vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
+function CC(index)
+	if index <= 0 or index > #themes then
+		return vim.notify("Could not change theme, index out of range. Current theme: " .. themes[theme_index])
+	end
+
+	SC(themes[index])
+
+	local theme = themes[index]
+
+	vim.notify("Changed color scheme to " .. theme)
+	theme_index = index
+end
+
 vim.cmd("TransparentEnable")
 
 SC()
