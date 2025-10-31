0b07b418ebb48a98225154cbb42e99de5f8f6af3
iicsx <maxinies00@gmail.com>
2025-10-31 18:02:20 +0100

    update(oil): add border property (rounded)
    plugin author defaulted borders to nil

--- extra/after/plugin/oil.lua
+++ extra/after/plugin/oil.lua
@@ -1,12 +1,13 @@
 require("oil").setup({
-  view_options = {
-    show_hidden = true,
-    natural_order = true
-  },
-  float = {
-    padding = 3
-  }
-});
+	view_options = {
+		show_hidden = true,
+		natural_order = true,
+	},
+	float = {
+		padding = 3,
+		border = "rounded",
+	},
+})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 vim.keymap.set("n", "_", "<CMD>:Oil --float<CR>", { desc = "Open parent directory in floating window" })
