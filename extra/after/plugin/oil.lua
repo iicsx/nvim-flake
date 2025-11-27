06eb1904a0b73178d77b19b36fef49b80e6c1380
iicsx <maxinies00@gmail.com>
2025-11-27 08:45:15 +0100

    update(oil): re-enable borders as per updated window options

--- extra/after/plugin/oil.lua
+++ extra/after/plugin/oil.lua
@@ -1,12 +1,16 @@
 require("oil").setup({
-	view_options = {
-		show_hidden = true,
-		natural_order = true,
-	},
-	float = {
-		padding = 3,
-		border = "rounded",
-	},
+  view_options = {
+    show_hidden = true,
+    natural_order = true,
+  },
+  float = {
+    padding = 3,
+    border = "rounded",
+  },
+  confirmation = {
+    padding = 3,
+    border = "rounded",
+  },
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
