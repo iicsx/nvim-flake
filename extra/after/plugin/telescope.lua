528c1b5b623e9ed255612d3b29ac173ae04e3366
iicsx <maxinies00@gmail.com>
2025-11-27 08:47:20 +0100

    update(telescope): add remap to open help tags and config files

--- extra/after/plugin/telescope.lua
+++ extra/after/plugin/telescope.lua
@@ -1,18 +1,28 @@
 local builtin = require('telescope.builtin')
 
+require("telescope").setup({
+  extensions = {
+    fzf = {}
+  }
+})
+
+require("telescope").load_extension("fzf")
+
 --vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 
 vim.keymap.set('n', '<leader>fa', function()
   builtin.find_files({
-    shorten_path = true, 
+    shorten_path = true,
     cwd = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:gs?\\?/?'),
     path_display = { 'truncate' }
   })
 end)
 
 vim.keymap.set('n', '<leader>ft', builtin.git_files, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fs', function() 
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
+vim.keymap.set('n', '<leader>fd', function()
+  builtin.find_files({
+    cwd = vim.fn.stdpath("config")
+  })
 end)
-
